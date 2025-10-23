c12b701bc4d4c6eb67255aefc6bb7d14046748d2
TaqdeesMehdi <taqdeesm99@gmail.com>
2025-10-23 11:49:18 +0500

    added the prisma with neon provided postgresql

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+const Home = async () => {
+  const users = await prisma.user.findMany();
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       <Button>Click the node</Button>
+      <h1>{JSON.stringify(users)}</h1>
     </div>
   );
-}
+};
+export default Home;
