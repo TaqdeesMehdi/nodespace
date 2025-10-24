ac9b02fd104e854b33df9414d744bdd90944a1f4
TaqdeesMehdi <taqdeesm99@gmail.com>
2025-10-24 18:29:08 +0500

    added the additional security for backend trpc protected procedure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
-const Home = () => {
-  const { data } = authClient.useSession();
+const Home = async () => {
+  await requireAuth();
+  const data = await caller.getUsers();
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       {JSON.stringify(data)}
-      {data && <Button onClick={() => authClient.signOut()}>Logout</Button>}
     </div>
   );
 };
