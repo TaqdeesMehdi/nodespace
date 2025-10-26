3c865c1bbe0a35c65211885a43d7ff6b22dea114
TaqdeesMehdi <taqdeesm99@gmail.com>
2025-10-26 18:36:28 +0500

    changed the theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
-import { requireAuth } from "@/lib/auth-utils";
+"use client";
 import { LogoutButton } from "@/components/logout-button";
-import { caller } from "@/trpc/server";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
 
-const Home = async () => {
-  await requireAuth();
-  const data = await caller.getUsers();
-
+const Home = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(data)}
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      <div>{JSON.stringify(data, null, 2)}</div>
       <LogoutButton />
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
     </div>
   );
 };
--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -2,11 +2,20 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export function LogoutButton() {
-  return (
-    <Button onClick={() => authClient.signOut()}>
-      Logout
-    </Button>
-  );
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/signup");
+        },
+      },
+    });
+  };
+
+  return <Button onClick={handleLogout}>Logout</Button>;
 }
