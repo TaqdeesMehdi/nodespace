1a2bf5b2a14ff0c89eb11c8d4b6d8ba45272361c
TaqdeesMehdi <taqdeesm99@gmail.com>
2026-02-18 21:00:08 +0500

    load workflow page bt Id

--- src/features/workflows/server/prefetch.ts
+++ src/features/workflows/server/prefetch.ts
@@ -1,6 +1,11 @@
 import { prefetch, trpc } from "@/trpc/server";
 import type { inferInput } from "@trpc/tanstack-react-query";
 type input = inferInput<typeof trpc.workflows.getMany>;
+//=======================prefetch all workflows
 export const prefetchWorkflows = (params: input) => {
   return prefetch(trpc.workflows.getMany.queryOptions(params));
+};
+//=======================prefetch a single workflow
+export const prefetchWorkflow = (id: string) => {
+  return prefetch(trpc.workflows.getOne.queryOptions({ id }));
 };
