5b4765b16653c995bf7bc222ada2f53bd00e03c5
TaqdeesMehdi <taqdeesm99@gmail.com>
2026-02-26 19:57:55 +0500

    dialog of http request node

---
+++ src/features/executions/components/http-request/dialog.tsx
@@ -0,0 +1,181 @@
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import z from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+export type FormType = z.infer<typeof formSchema>;
+const formSchema = z.object({
+  endpoint: z.url({ message: "Please enter a valid URL" }),
+  method: z.enum(["GET", "POST", "PUT", "PATCH", "DELETE"]),
+  body: z.string().optional(),
+});
+interface HttpRequestDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onSubmit: (values: z.infer<typeof formSchema>) => void;
+  defaultEndpoint?: string;
+  defaultMethod?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  defaultBody?: string;
+}
+export const HttpRequestDialog = ({
+  open,
+  onOpenChange,
+  onSubmit,
+  defaultEndpoint = "",
+  defaultBody = "",
+  defaultMethod = "GET",
+}: HttpRequestDialogProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      endpoint: defaultEndpoint,
+      method: defaultMethod,
+      body: defaultBody,
+    },
+  });
+  useEffect(() => {
+    if (open) {
+      form.reset({
+        endpoint: defaultEndpoint,
+        method: defaultMethod,
+        body: defaultBody,
+      });
+    }
+  }, [open, defaultBody, defaultMethod, defaultEndpoint, form]);
+  const watchMethod = form.watch("method");
+  const showBodyField = ["POST", "PUT", "PATCH"].includes(watchMethod);
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    onSubmit(values);
+    onOpenChange(false);
+  };
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="max-h-[90vh] overflow-y-auto">
+        <DialogHeader>
+          <DialogTitle>Http Request</DialogTitle>
+          <DialogDescription>
+            Configure settings for the HTTP Request node.
+          </DialogDescription>
+        </DialogHeader>
+        {/* <div className="py-4">
+          <p className="text-sm text-muted-foreground">
+            This allows you to configure and send HTTP requests by specifying
+            endpoint URL, method type, and optional request body.
+          </p>
+        </div> */}
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-8 mt-4"
+          >
+            {/****request method field*/}
+            <FormField
+              control={form.control}
+              name="method"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Method</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="w-full ">
+                        <SelectValue placeholder="Select a method"></SelectValue>
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="GET">GET</SelectItem>
+                      <SelectItem value="POST">POST</SelectItem>
+                      <SelectItem value="PUT">PUT</SelectItem>
+                      <SelectItem value="PATCH">PATCH</SelectItem>
+                      <SelectItem value="DELETE">DELETE</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormDescription>
+                    The HTTP method to use for this request.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/****endpoint field */}
+            <FormField
+              control={form.control}
+              name="endpoint"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Endpoint URL</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="https://api.example.com/users/{{httpResponse.data.id}}"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Static URL or use {"{{variables}}"} for simple values or{" "}
+                    {"{{json variables}}"} to stringify objects
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {showBodyField && (
+              <FormField
+                control={form.control}
+                name="body"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Request Body</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder={`{\n  "name": "{{userName}}",\n  "email": "{{userEmail}}",\n  "metadata": {{json userData}}\n}`}
+                        {...field}
+                        className="min-h-30 font-mono text-sm"
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Static URL or use {"{{variables}}"} for simple values or{" "}
+                      {"{{json variables}}"} to stringify objects
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+            <DialogFooter className="mt-4">
+              <Button type="submit">Save</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -1,8 +1,9 @@
 "use client";
-import type { Node, NodeProps, useReactFlow } from "@xyflow/react";
+import { Node, NodeProps, useReactFlow } from "@xyflow/react";
 import { GlobeIcon } from "lucide-react";
 import { memo, useState } from "react";
 import { BaseExecutionNode } from "../base-execution-node";
+import { FormType, HttpRequestDialog } from "./dialog";
 type HttpRequestNodeData = {
   endpoint?: string;
   method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
@@ -12,13 +13,42 @@ type HttpRequestNodeData = {
 type HttpRequestNodeType = Node<HttpRequestNodeData>;
 
 export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const { setNodes } = useReactFlow();
+  const nodeStatus = "initial";
+  const handleOpenSettings = () => setDialogOpen(true);
+  const handleSubmit = (values: FormType) => {
+    setNodes((nodes) =>
+      nodes.map((node) => {
+        if (node.id === props.id) {
+          return {
+            ...node,
+            data: {
+              ...node.data,
+              endpoint: values.endpoint,
+              method: values.method,
+              body: values.body,
+            },
+          };
+        }
+        return node;
+      }),
+    );
+  };
   const nodeData = props.data;
   const description = nodeData?.endpoint
     ? `${nodeData.method || "GET"}: ${nodeData.endpoint}`
     : "Not Configured";
-  const nodeStatus = "initial";
   return (
     <>
+      <HttpRequestDialog
+        open={dialogOpen}
+        onOpenChange={setDialogOpen}
+        onSubmit={handleSubmit}
+        defaultEndpoint={nodeData.endpoint}
+        defaultMethod={nodeData.method}
+        defaultBody={nodeData.body}
+      />
       <BaseExecutionNode
         {...props}
         id={props.id}
@@ -26,8 +56,8 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
         name="HTTP Request"
         status={nodeStatus}
         description={description}
-        onSettings={() => {}}
-        onDoubleClick={() => {}}
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
       />
     </>
   );
